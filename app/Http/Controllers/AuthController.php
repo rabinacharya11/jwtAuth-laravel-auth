d175a9aed9259dca4c1dd8f89460f7b14c00675a
Rabin Acharya <charms2074@gmail.com>
2022-08-04 21:31:08 +0545

    final authentication process done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Validator;
 
 
